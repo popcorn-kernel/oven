53e60a116f4e8216a763fe087094c3a6041312e2
K. Y. Cemal Öztürk <info@yigitovski.com>
2023-07-27 19:39:54 +0300

    Switch to rust (#7)
    * Switch to Rust
    
    * switch to rust

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+// main.rs - entry point for the cuteloader
+
+#![no_std]
+
+
+fn main() {
+}
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
